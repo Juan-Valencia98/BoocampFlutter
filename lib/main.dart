c5f33bf1da68bf9da04a73b452a794c52582036c
Juan Valencia <67487522+Juan-Valencia98@users.noreply.github.com>
2021-11-18 19:24:21 -0400

    Hola Mundo1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
 }
